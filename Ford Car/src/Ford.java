8e3c0fb9a75ccefb5599750fa0f683657a569276
Gulfam <Gulfam@Gulfam-HP>
2021-09-28 22:34:18 -0500

    Commit

---
+++ Ford Car/src/Ford.java
@@ -0,0 +1,25 @@
+public class Ford {
+
+	public static void main(String[] args) {
+
+		int myfordyear = 2017;
+		String myfordmake = "Ford";
+		String myfordmodel = "Fusion";
+		String fordspecs = "Hybrid";
+		String color = "Gray";
+
+		int a = myfordyear;
+		String b = myfordmake;
+		String c = myfordmodel;
+		String d = fordspecs;
+		String e = color;
+
+		System.out.println("The Year is a: " + a);
+		System.out.println("This Make is a: " + b);
+		System.out.println("The Model is a: " + c);
+		System.out.println("The Configuration is a: " + d);
+		System.out.println("The Car color is: " + e);
+
+	}
+
+}
